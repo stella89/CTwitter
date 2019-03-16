fb6d4b050e498de2f2fa207f189cf668bbeb3447
Carine Domingo <cdomingo@spectrumdt.com>
2019-03-16 00:49:15 -0400

    Initial Commit

---
+++ CTwitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CTwitter
+//
+//  Created by Djivede on 2019-03-16.
+//  Copyright © 2019 spectrumdt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
